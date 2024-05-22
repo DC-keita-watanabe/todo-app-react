778c42514f937e00ed30800acf17745ae344ee45
keita.watanabe <keita.watanabe@g.dreamcareer.co.jp>
2024-05-22 10:36:39 +0900

    EditコンポーネントのRouteを設定しました

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TaskList from './components/TaskList';
 import AddTask from './components/AddTask';
 import DeleteTask from './components/DeleteTask';
+import EditTask from './components/EditTask';
+
 
 function App() {
   return (
@@ -9,7 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<TaskList />} />
         <Route path="/create" element={<AddTask />} />
-        <Route path="/delete/:id" element={<DeleteTask />} />
+        <Route path="/delete/" element={<DeleteTask />} />
+        <Route path="/edit/" element={<EditTask />} />
       </Routes>
     </Router>
   );
